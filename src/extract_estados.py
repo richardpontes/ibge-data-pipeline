297ea24f0986df86af79cba07e9a0c57c5953d79
Richard <richardpontes@gmail.com>
2026-06-11 23:39:51 -0300

    Implementa exportacao de estados para CSV

--- src/extract_estados.py
+++ src/extract_estados.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 
 url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
 
@@ -6,10 +7,17 @@ resposta = requests.get(url)
 
 dados = resposta.json()
 
-for estado in dados:
-    print(
-        estado["id"],
-        estado["sigla"],
-        estado["nome"],
-        estado["regiao"]["nome"]
-    )
+with open("data/estados.csv", mode="w", encoding="utf-8", newline="") as arquivo:
+    escritor = csv.writer(arquivo)
+
+    escritor.writerow(["id", "sigla", "nome", "regiao"])
+
+    for estado in dados:
+        escritor.writerow([
+            estado["id"],
+            estado["sigla"],
+            estado["nome"],
+            estado["regiao"]["nome"]
+        ])
+
+print("Arquivo estados.csv gerado com sucesso!")
